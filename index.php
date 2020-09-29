13ba488079e477365da26e4ee4bf828bafd67379
VittorioBarella <vittorio.br@hotmail.com>
2020-09-29 01:05:31 -0300

    Implementação da condicional para pratos em destaque.

--- index.php
+++ index.php
@@ -85,8 +85,69 @@
                 <hr>
                 </hr>
             </div>
+            <div class="slider-cardapio">
+                <div class="slider-002 small-12 small-centered columns">
+                    <?php
+
+                    $server = 'localhost';
+                    $user = 'root';
+                    $password = '';
+                    $db_name = 'restaurante';
+                    $port = '3306';
+
+                    $db_connect = new mysqli($server, $user, $password, $db_name, $port);
+                    // Este comando é para inserir os dados com os acentos corretamente.
+                    mysqli_set_charset($db_connect, "utf8");
+
+                    if ($db_connect->connect_error) {
+                        echo 'Falha: ' . $db_connect->connect_error;
+                    } else {
+                        // echo 'Conexão feita com sucesso' . '<br><br>';
+                        $sql = "SELECT * from pratos WHERE destaque =1";
+                        $result = $db_connect->query($sql);
+
+                        if ($result->num_rows > 0) {
+
+                            while ($row = $result->fetch_assoc()) { ?>
+                                <div class="cardapio-item-outer bounce-hover small-10 medium-4 columns">
+                                    <div class="cardapio-item">
+                                        <a href="prato.php?prato=
+                                        <?php
+                                        echo $row['codigo'];
+                                        ?>">
+
+                                            <div class="cardapio-item-image">
+                                                <img src="img/cardapio/
+                                                <?php
+                                                echo $row['codigo'];
+                                                ?>.jpg" alt="camarao" />
+                                            </div>
+
+                                            <div class="item-info">
 
 
+                                                <div class="title">
+                                                    <?php
+                                                    echo $row['nome'];
+                                                    ?></div>
+                                            </div>
+
+                                            <div class="gradient-filter">
+                                            </div>
+
+                                        </a>
+                                    </div>
+                                </div>
+
+                    <?php }
+                        } else {
+                            'Não há destaques';
+                        }
+                    }
+                    ?>
+
+                </div>
+            </div>
             <div class="reservation-form small-12 columns no-padding">
 
                 <form>
